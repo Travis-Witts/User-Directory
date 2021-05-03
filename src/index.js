d1f4260f901790d23d04fd055ff01acc58680e41
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-03 16:12:33 +0930

    fixed issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
